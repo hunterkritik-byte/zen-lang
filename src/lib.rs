13cdf1b98090d338a8a9cf3a86a4d5b6b242d540
KRITIK BHATTARAI <hunterkritik@gmail.com>
2026-09-15 04:41:04 -0700

    refactor: expose compiler diagnostics and type checker

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-pub mod lexer;
 pub mod ast;
+pub mod lexer;
 pub mod parser;
 pub mod runtime;
+pub mod span;
+pub mod typeck;
 
 pub use lexer::{lex, Token};
 pub use parser::parse;
 pub use runtime::run;
+pub use typeck::check;
